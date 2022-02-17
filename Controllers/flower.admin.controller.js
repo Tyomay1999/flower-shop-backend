4a92ab7df03a7f5b4358fe01dfa403671480dc8d
Tyomay1999 <artyom.bordulanyk@gmail.com>
2022-02-17 13:01:00 -0800

    add pagination

--- Controllers/flower.admin.controller.js
+++ Controllers/flower.admin.controller.js
@@ -5,6 +5,7 @@ const path = require( 'path' )
 const uuid = require( 'uuid' )
 const sequelize = require( 'sequelize' )
 const { Op } = require( "sequelize" );
+const { rows } = require( "pg/lib/defaults" );
 
 // categories = JSON.parse(categories)
 // console.log(typeof JSON.parse(tags))
@@ -39,9 +40,65 @@ const { Op } = require( "sequelize" );
 
 
 class FlowerAdminController {
-    async getAll( req, res ) {
-        const flowers = await Flower.findAll()
-        return await res.status( 200 ).json( { data: flowers } )
+    async getAll( req, res, next ) {
+    try{
+        let { category_id, prices, limit, page } = req.body
+        prices = prices || [0, 15]
+        limit = limit || 10
+        page = page || 1
+        const offset = page * limit - limit
+        if(category_id && !prices.length){
+            const category = await Categories.findOne({where: {id: category_id}})
+            const flowers = await Flower.findAndCountAll({
+                where: {
+                    id: {
+                        [Op.or] : category.flower_ids
+                    }
+                },
+                limit,
+                offset
+            })
+            return res.status(200).json({count: flowers.count,flowers: flowers.rows})
+        }
+        if(!category_id && prices.length){
+            prices = JSON.parse(prices)
+            const filteredFlowers = []
+            const flowers = await Flower.findAndCountAll({ limit, offset })
+             flowers.rows.forEach(flower => {
+                if(prices[0] < flower.price && flower.price < prices[1]) {
+                   filteredFlowers.push({flower})
+                }
+            })
+            return res.status(200).json({count: flowers.count,flowers: filteredFlowers})
+        }
+        if(category_id && prices.length){
+            prices = JSON.parse(prices)
+            const filteredFlowers = []
+            const category = await Categories.findOne({where: {id: category_id}})
+            const flowers = await Flower.findAndCountAll({
+                where: {
+                    id: {
+                        [Op.or] : category.flower_ids
+                    }
+                },
+                limit,
+                offset
+            })
+            flowers.rows.forEach(flower => {
+                if(prices[0] < flower.price && flower.price < prices[1]) {
+                    filteredFlowers.push(flower)
+                }
+            })
+            return res.status(200).json({count: flowers.count,flowers: filteredFlowers})
+        }
+        if(!category_id && !prices.length){
+            const flowers = await Flower.findAndCountAll({limit,offset})
+            return res.status(200).json({count: flowers.count, flowers: flowers.rows})
+        }
+    } catch ( error ) {
+        next(apiError.badRequest(error.message))
+    }
+
     }
 
     async addFlower( req, res, next ) {
@@ -52,7 +109,7 @@ class FlowerAdminController {
             await photo.mv( path.resolve( __dirname, '..', 'static', filename ) )
             // categories = JSON.parse(categories)
             // tags = JSON.parse(tags)
-            categories = [ 1, 2, 3 ]
+            categories = [ 1, 2, 6, 9 ]
             tags = [ 'tag1', 'tag2', 'tag3', 'tag4' ]
             const flower = await Flower.create( {
                 name,
--- Routes/AdminRoutes/flower.admin.router.js
+++ Routes/AdminRoutes/flower.admin.router.js
@@ -3,7 +3,7 @@ const FlowerAdminController = require('../../Controllers/flower.admin.controller
 
 const router = new Router()
 
-router.get('/all', FlowerAdminController.getAll)
+router.post('/all', FlowerAdminController.getAll)
 router.post('/add', FlowerAdminController.addFlower)
 router.put('/edit', FlowerAdminController.editFlower)
 router.delete('/delete', FlowerAdminController.deleteFlower)
