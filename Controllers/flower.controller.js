7e11f7913036fa6833bd331b7815eef895dd4e9e
Tyomay1999 <artyom.bordulanyuk@beeoncode.com>
2022-05-18 11:46:39 +0300

    add all flower routes

--- Controllers/flower.controller.js
+++ Controllers/flower.controller.js
@@ -1,34 +1,34 @@
-const apiError = require('../Error/apiError')
-const {Categories, Flower} = require("../Models/model");
-const {Op} = require('sequelize')
+const apiError = require( '../Error/apiError' )
+const { Categories, Flower } = require( "../Models/model" );
+const { Op } = require( 'sequelize' )
 
 //TODO need to change
 const handlerIdSorts = async data => {
     const idsArray = []
-    data.forEach(item => {
-        item.flower_ids?.forEach(id => {
-            if (idsArray.length) {
+    data.forEach( item => {
+        item.flower_ids?.forEach( id => {
+            if ( idsArray.length ) {
                 let haveId = 0;
-                idsArray.forEach(selectedId => {
-                    if (selectedId === id) {
+                idsArray.forEach( selectedId => {
+                    if ( selectedId === id ) {
                         haveId = 1
                     }
-                })
-                if (!haveId) {
-                    idsArray.push(id)
+                } )
+                if ( !haveId ) {
+                    idsArray.push( id )
                 }
             }
-            if (!idsArray.length) {
-                idsArray.push(id)
+            if ( !idsArray.length ) {
+                idsArray.push( id )
             }
-        })
-    })
+        } )
+    } )
     return idsArray;
 }
 
 
 class FlowerController {
-    async allFlowers(req, res, next) {
+    async allFlowers( req, res, next ) {
         try {
             let { category_id, prices, limit, page } = req.body
             prices = prices || [ 0, 15 ]
@@ -46,7 +46,7 @@ class FlowerController {
                     limit,
                     offset
                 } )
-                console.log( 'line 30 fl.ad.co', offset , '<-offset', page, '<-page', limit,'<-limit' )
+                console.log( 'line 30 fl.ad.co', offset, '<-offset', page, '<-page', limit, '<-limit' )
                 return res.status( 200 ).json( { count: flowers.count, flowers: flowers.rows } )
             }
             if ( !category_id && prices.length ) {
@@ -54,8 +54,8 @@ class FlowerController {
                 const flowers = await Flower.findAndCountAll( {
                     where: {
                         price: {
-                            [ Op.gte ]: prices[0],
-                            [ Op.lte ]: prices[1],
+                            [ Op.gte ]: prices[ 0 ],
+                            [ Op.lte ]: prices[ 1 ],
                         }
                     },
                     limit,
@@ -65,12 +65,12 @@ class FlowerController {
                     if ( product.categories.length ) {
                         const categoriesArray = []
                         product.categories.forEach( categoryId => {
-                            categoriesArray.push( categories.filter( category => category.id === categoryId )[0]?.name )
+                            categoriesArray.push( categories.filter( category => category.id === categoryId )[ 0 ]?.name )
                         } )
                         product.categories = categoriesArray
                     }
                 } )
-                console.log( 'line 42 fl.ad.co', offset , '<-offset', page, '<-page', limit,'<-limit' )
+                console.log( 'line 42 fl.ad.co', offset, '<-offset', page, '<-page', limit, '<-limit' )
                 return res.status( 200 ).json( { count: flowers.count, flowers: flowers.rows } )
             }
             if ( category_id && prices.length ) {
@@ -81,129 +81,140 @@ class FlowerController {
                             [ Op.or ]: category.flower_ids
                         },
                         price: {
-                            [ Op.gte ]: prices[0],
-                            [ Op.lte ]: prices[1],
+                            [ Op.gte ]: prices[ 0 ],
+                            [ Op.lte ]: prices[ 1 ],
                         }
                     },
                     limit,
                     offset
                 } )
-                console.log( 'line 59 fl.ad.co', offset , '<-offset', page, '<-page', limit,'<-limit', prices, "<-prices" )
+                console.log( 'line 59 fl.ad.co', offset, '<-offset', page, '<-page', limit, '<-limit', prices, "<-prices" )
                 return res.status( 200 ).json( { count: flowers.count, flowers: flowers.rows } )
             }
             if ( !category_id && !prices.length ) {
                 const flowers = await Flower.findAndCountAll( { limit, offset } )
-                console.log( 'line 69 fl.ad.co', offset , '<-offset', page, '<-page', limit,'<-limit' )
+                console.log( 'line 69 fl.ad.co', offset, '<-offset', page, '<-page', limit, '<-limit' )
                 return res.status( 200 ).json( { count: flowers.count, flowers: flowers.rows } )
             }
         } catch ( error ) {
             next( apiError.badRequest( error.message ) )
         }
     }
-    async getByIds (req, res, next) {
+
+    async getByIds( req, res, next ) {
         try {
             let flowers = []
-            if(req.body.flower_ids?.length){
-                flowers = await Flower.findAll({
+            if ( req.body.flower_ids?.length ) {
+                flowers = await Flower.findAll( {
                     where: {
                         id: {
-                            [Op.or]: JSON.parse(req.body.flower_ids)
+                            [ Op.or ]: JSON.parse( req.body.flower_ids )
                         }
                     }
-                })
-                return res.status(200).json({flowers})
+                } )
+                return res.status( 200 ).json( { flowers } )
             }
         } catch ( error ) {
-            next(apiError.badRequest(error.message))
+            next( apiError.badRequest( error.message ) )
         }
     }
-    async getSimilar(req, res, next) {
-        console.log(req.body.categories,'<------categories')
+
+    async getAll( req, res, next ) {
         try {
-            const {categories, page, limit = 6} = req.body
+            let flowers = await Flower.findAndCountAll()
+            return res.status( 200 ).json( { flowers } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
+        }
+    }
+
+    async getSimilar( req, res, next ) {
+        console.log( req.body.categories, '<------categories' )
+        try {
+            const { categories, page, limit = 6 } = req.body
             const offset = page * limit - limit
-            const similarFlowersCategories = await Categories.findAll({
+            const similarFlowersCategories = await Categories.findAll( {
                 where: {
                     id: {
-                        [Op.or]: JSON.parse(categories)
+                        [ Op.or ]: JSON.parse( categories )
                     }
                 }
-            })
-            const flowersId = await handlerIdSorts(similarFlowersCategories)
-            const data = await Flower.findAndCountAll({
+            } )
+            const flowersId = await handlerIdSorts( similarFlowersCategories )
+            const data = await Flower.findAndCountAll( {
                 where: {
                     id: {
-                        [Op.or]: flowersId
+                        [ Op.or ]: flowersId
                     }
                 },
                 limit,
                 offset
-            })
-            return res.status(200).json({count: data.count ,similarFlowers: data.rows})
-        } catch (error) {
-            next(apiError.badRequest(error.message))
+            } )
+            return res.status( 200 ).json( { count: data.count, similarFlowers: data.rows } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
         }
     }
 
-    async getNewFlowers(req, res, next) {
+    async getNewFlowers( req, res, next ) {
         try {
             let { limit, page } = req.body
             limit = limit || 9;
             page = page || 1;
             const offset = page * limit - limit
-            const newFlowers = await Flower.findAndCountAll({
+            const newFlowers = await Flower.findAndCountAll( {
                 where: { isNew: true },
                 limit,
                 offset
-            })
-            return res.status(200).json({count: newFlowers.count, flowers: newFlowers.rows})
-        } catch (error) {
-            next(apiError.badRequest(error.message))
+            } )
+            return res.status( 200 ).json( { count: newFlowers.count, flowers: newFlowers.rows } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
         }
     }
 
-    async getOne(req, res, next) {
+    async getOne( req, res, next ) {
         try {
-            const {slug} = req.params
-            if (!slug) {
-                return next(apiError.badRequest('Slug not found'))
+            const { slug } = req.params
+            if ( !slug ) {
+                return next( apiError.badRequest( 'Slug not found' ) )
             }
-            const flower = await Flower.findOne({where: {slug}})
-            return res.status(200).json({flower})
-        } catch (error) {
-            next(apiError.badRequest(error.message))
+            const flower = await Flower.findOne( { where: { slug } } )
+            return res.status( 200 ).json( { flower } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
         }
     }
 
-    async getFlowersWithIds(req, res, next) {
+    async getFlowersWithIds( req, res, next ) {
         try {
-            const {flowerIds} = req.body
-            console.log(JSON.parse(flowerIds))
-            let flowers = await Flower.findAll({
+            const { flowerIds } = req.body
+            console.log( JSON.parse( flowerIds ) )
+            let flowers = await Flower.findAll( {
                 where: {
                     id: {
-                        [Op.or] : JSON.parse(flowerIds)
+                        [ Op.or ]: JSON.parse( flowerIds )
                     }
                 }
-            })
-            return res.status(200).json({flowers})
-        } catch (error) {
-            next(apiError.badRequest(error.message))
+            } )
+            return res.status( 200 ).json( { flowers } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
         }
     }
 
-    async getSearchedFlowers (req, res, next){
+    async getSearchedFlowers( req, res, next ) {
         try {
-            const {name} = req.body
-            console.log(name,'name')
-            let flowers = await Flower.findAndCountAll({
+            const { name } = req.body
+            console.log( name, 'name' )
+            let flowers = await Flower.findAndCountAll( {
                 where: {
                     name: name
                 }
-            })
-            return res.status(200).json({count: flowers.count ,flowers: flowers.rows})
-        } catch (error) {
-            next(apiError.badRequest(error.message))
+            } )
+            return res.status( 200 ).json( { count: flowers.count, flowers: flowers.rows } )
+        } catch ( error ) {
+            next( apiError.badRequest( error.message ) )
         }
     }
 }
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 require( 'dotenv' ).config()
 const express = require( 'express' )
-const sequelize = require('./db')
-const fileUpload = require('express-fileupload')
-const models = require('./Models/model')
-const cors = require('cors')
-const routers = require('./Routes/index')
+const sequelize = require( './db' )
+const fileUpload = require( 'express-fileupload' )
+const models = require( './Models/model' )
+const cors = require( 'cors' )
+const routers = require( './Routes/index' )
 const PORT = process.env.PORT || 5000
-const errorHandler = require('./Middleware/errorHandlingMiddleware')
-const path = require('path')
+const errorHandler = require( './Middleware/errorHandlingMiddleware' )
+const path = require( 'path' )
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload())
-app.use('/api', routers)
+app.use( cors() )
+app.use( express.json() )
+app.use( express.static( path.resolve( __dirname, 'static' ) ) )
+app.use( fileUpload() )
+app.use( '/api', routers )
+app.get( '/', ( req, res ) => {
+    return res.status( 200 ).send( `
+        <div style='width: 100%; height: 100vh;display: flex;justify-content: center;align-items: center'>
+            <h1>API is working</h1>
+        </div>
+` )
+} )
 
-app.use(errorHandler)
+app.use( errorHandler )
 const start = async () => {
     try {
         await sequelize.authenticate()
